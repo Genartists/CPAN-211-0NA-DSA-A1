0e595d3dd718dd3cb89ace7fa727e5a5e3497815
avnii21 <n01266557@humbermail.ca>
2025-05-20 21:27:57 -0400

    Add Triangle class and update GeometricObject

---
+++ GeometryProject/GeometryProject/src/Geometry/GeometricObject.java
@@ -0,0 +1,35 @@
+package Geometry;
+
+public abstract class GeometricObject {
+    private String color = "white";
+    private boolean filled;
+
+    public GeometricObject() {
+        // Default constructor
+    }
+
+    protected GeometricObject(String color, boolean filled) {
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    public abstract double getArea();
+
+    public abstract double getPerimeter();
+}
---
+++ GeometryProject/GeometryProject/src/Geometry/Triangle.java
@@ -0,0 +1,53 @@
+package Geometry;
+
+public class Triangle extends GeometricObject {
+    private double side1 = 1.0;
+    private double side2 = 1.0;
+    private double side3 = 1.0;
+
+    // no-arg constructor that creates a default triangle
+    public Triangle() {
+    }
+
+    // the constructor with specified side lengths
+    public Triangle(double side1, double side2, double side3) {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    // for side1
+    public double getSide1() {
+        return side1;
+    }
+
+    // for side2
+    public double getSide2() {
+        return side2;
+    }
+
+    // for side3
+    public double getSide3() {
+        return side3;
+    }
+
+    // calculating perimeter of the triangle
+    @Override
+    public double getPerimeter() {
+        return side1 + side2 + side3;
+    }
+
+    // calculating the area w/ given formula
+    @Override
+    public double getArea() {
+        double s = getPerimeter() / 2.0;
+        return Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
+    }
+
+    @Override
+    public String toString() {
+        return "Triangle: side1 = " + side1 + 
+               ", side2 = " + side2 + 
+               ", side3 = " + side3;
+    }
+}
